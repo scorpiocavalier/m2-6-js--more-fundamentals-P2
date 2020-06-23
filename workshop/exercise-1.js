fc55cd98602b72a846f1416218509f37cc9b049b
Arturo Chen <art.can.fly@gmail.com>
2020-06-23 14:51:59 -0400

    E1 done

--- workshop/exercise-1.js
+++ workshop/exercise-1.js
@@ -2,13 +2,31 @@
 // ----------
 
 let mostPopularFood = [
-  'poke bowl',
-  'chicken sliders',
-  'pork baby back ribs',
-  'chicken burritos',
-  'chicken sandwich',
-  'cauliflower rice bowl',
-  'chicken and waffle sliders',
-  'parmesan chicken',
+  "poke bowl",
+  "chicken sliders",
+  "pork baby back ribs",
+  "chicken burritos",
+  "chicken sandwich",
+  "cauliflower rice bowl",
+  "chicken and waffle sliders",
+  "parmesan chicken",
   undefined,
 ];
+
+const fixList = (arr, first, last) => {
+  arr.unshift(first);
+  arr.push(last);
+  return arr;
+};
+
+const printList = (arr) => {
+  let index = 1;
+  arr.forEach((food) => {
+    if (food != undefined)
+      console.log(`${index++}. ${food}`);
+  });
+};
+
+printList(
+  fixList(mostPopularFood, "bean burritos", "buffalo-flavored cauliflower")
+);
