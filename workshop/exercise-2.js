96afab1e999558d3509490d69432feb66ff02e69
Arturo Chen <art.can.fly@gmail.com>
2020-06-23 14:55:42 -0400

    E2 done

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -3,3 +3,9 @@
 
 const lateComers = ['Felicia', 'Charles', 'Terry'];
 const earlyBirds = ['Ellen', 'Jerry', 'Jasmine'];
+
+const printGuestOrder = (early, late) => {
+    [...earlyBirds, ...lateComers].forEach(person => console.log(person))
+}
+
+printGuestOrder(earlyBirds, lateComers)
